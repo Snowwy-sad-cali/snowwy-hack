78495de5693270c9348207fe54125748422e29ed
Snowwy-sad-cali <58283456+Snowwy-sad-cali@users.noreply.github.com>
2019-12-01 11:11:00 +1100

    Update SnowwyLoader.js

--- loader/SnowwyLoader.js
+++ loader/SnowwyLoader.js
@@ -7,7 +7,7 @@
 */
 
 try {
-	(ttap)(hrt);
+	(Yale)(Snowwy);
 } catch(e) {
 	try {
 		let recursing = e.stack.match(/Snowwyloader/g).length > 1;
@@ -15,24 +15,24 @@ try {
 			// must be synchronous to force execution before other scripts
 			// note: we fetch the same code for each iframe
 			let request = new XMLHttpRequest();
-			request.open('GET', 'https://raw.githubusercontent.com/Snowwy-sad-cali/snowwy-hack/master/Snowwy hek min.js', false);
+			request.open('GET', 'https://raw.githubusercontent.com/Snowwy-sad-cali/snowwy-hack/master/Snowwy hek .min.js', false);
 			request.send(null);
 			if (request.status != 200) {
-				console.error('Error GET Snowwy Hack: ' + request.status);
+				console.error('Error GET snowwy-hack: ' + request.status);
 			}
 
 			const unique_string = chrome.runtime.getURL('').match(/\/\/(\w{9})\w+\//)[1];
-			let code = request.responseText.replace(/ttap#4547/g, unique_string);
+			let code = request.responseText.replace(/𝙎𝙖𝙙 𝘾𝙖𝙡𝙞  ™#5393/g, unique_string);
 
 			// inject our code into a new iframe to avoid using hooks placed by anti cheat
 			let frame = document.createElement('iframe');
 			frame.setAttribute('style', 'display:none');
 			document.documentElement.appendChild(frame);
-			let child = frame.contentDocument || frame.contentWindow.document;
-			let Snowwy = document.createElement('script');
-			Snowwy.innerHTML = code;
-			child.documentElement.append(Snowwy);
-			child.documentElement.remove(Snowwy);
+			let Yale = frame.contentDocument || frame.contentWindow.document;
+			let snowwy = document.createElement('script');
+			snowwy.innerHTML = code;
+			Yale.documentElement.append(snowwy);
+			Yale.documentElement.remove(snowwy);
 			document.documentElement.removeChild(frame);
 		}
 	} catch (e) {
